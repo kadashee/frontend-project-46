15e5724237db07797accf69da72767a0da264612
kadashee <mikita.mazeika@yandex.ru>
2025-02-28 21:46:51 +0300

    Added argument and format change in getdiff help

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,10 +3,13 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .name('gendiff')
-  .description('Compares two configuration files and shows a difference')
-  .version('0.0.1')
-  .helpOption('-h, --help', 'output usage information');
+    .name('gendiff')
+    .description('Compares two configuration files and shows a difference')
+    .version('0.0.1')
+    .helpOption('-h, --help', 'output usage information')
+    .argument('<filepath1>')
+    .argument('<filepath2>')
+    .option('-f, --format [type]', 'output format');
 
 program.parse()
 
