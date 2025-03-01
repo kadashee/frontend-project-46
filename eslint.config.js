e20a685081984b1aaada955bb138ef365049504c
kadashee <mikita.mazeika@yandex.ru>
2025-03-01 13:14:34 +0300

    Added changes

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,25 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
-import eslintPluginJest from 'eslint-plugin-jest';
+import jestPlugin from 'eslint-plugin-jest';
 
 export default [
   pluginJs.configs.recommended,
-  eslintPluginJest.configs.recommended,
   {
     languageOptions: {
       globals: {
         ...globals.node,
       },
     },
+  },
+  {
     files: ['**/*.test.js', '**/__tests__/**/*.js'],
-    ...eslintPluginJest.configs.recommended,
+    plugins: {
+      jest: jestPlugin,
+    },
+    languageOptions: {
+      globals: {
+        ...jestPlugin.environments.globals.globals,
+      },
+    },
   },
 ];
