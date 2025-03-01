bbc7e696bd42415a36df900e7d17980e91af3f0e
kadashee <mikita.mazeika@yandex.ru>
2025-03-02 00:09:39 +0300

    minor test changes

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -14,12 +14,12 @@ const file1 = parseFile(getFixturePath('file1.json'));
 const file2 = parseFile(getFixturePath('file2.json'));
 
 const expected = `{
-    - follow: false
-    host: hexlet.io
-    - proxy: 123.234.53.22
-    - timeout: 50
-    + timeout: 20
-    + verbose: true
+  - follow: false
+  host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
 }
 `;
 
